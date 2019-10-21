86ae81b89d6e49a5bfdbadb5bc5af8367cbf91a8
hemanthkumar7 <53857478+hemanthkumar7@users.noreply.github.com>
2019-10-21 16:52:32 +0530

    Create file.asm

---
+++ file.asm
@@ -0,0 +1,56 @@
+.model small
+display macro msg
+        lea dx,msg
+        mov ah,09h
+        int 21h
+endm
+.data
+msg1 db 0dh,0ah,"enter the file name : $"
+msg2 db 0dh,0ah,"file created successfully$"
+msg3 db 0dh,0ah,"creation failed$"
+msg4 db 0dh,0ah,"enter the filename to delete:$"
+msg5 db 0dh,0ah,"deleted successfully$"
+msg6 db 0dh,0ah,"deletion failed$"
+fname db 40h dup(?)
+fname2 db 40h dup(?)
+.code
+start:mov ax,@data
+      mov ds,ax
+      display msg1
+      mov si,offset fname
+back:mov ah,01h
+     int 21h
+     cmp al,0dh
+     je next
+     mov [si],al
+     inc si
+     jmp back
+next:mov [si],byte ptr '$'
+     lea dx,fname
+     mov cx,00h
+     mov ah,3ch
+     int 21h
+     jc failed
+     display msg2
+     jmp next1
+failed: display msg3
+next1:display msg4
+      mov si,offset fname2
+back1:mov ah,01h
+      int 21h
+      cmp al,0dh
+      je next2
+      mov [si],al
+      inc si
+      jmp back1
+next2:mov [si],byte ptr '$'
+      lea dx,fname2
+      mov ah,41h
+      int 21h
+      jc dfail
+      display msg5
+      jmp finAL
+dfail: display msg6
+final:mov ah,4ch
+      int 21h
+end start
